fa6d88c8bd6e78fcb357915f12374d3841b0701b
mjsrinivas <130982914+mjsrinivas@users.noreply.github.com>
2023-06-29 16:37:18 +0530

    commit

---
+++ Balance.java
@@ -0,0 +1,18 @@
+package Banking;
+
+public class Balance
+{
+	void meth1()
+	{
+		System.out.println("i love coding");
+	}
+
+	public static void main(String[] args)
+	{
+     System.out.println("Hello world");	
+     Balance sandy=new Balance();
+     sandy.meth1();
+
+	}
+
+}
